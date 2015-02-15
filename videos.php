5985d3d98f2b5b312b7581f79af47e99b4535022
thalijw <thalijw@onid.oregonstate.edu>
2015-02-15 09:12:27 -0800

    Final submit

--- videos.php
+++ videos.php
@@ -45,6 +45,16 @@ if (isset($_POST['switch']) && isset($_POST['id'])){
 	}
 }
 
+if (isset($_POST['deleteAll'])){
+
+	$query = "TRUNCATE TABLE `Inventory` ";
+	$result	= $mysqli->query($query);
+	if (!$result) {
+		die ("Database access failed: " . $mysqli->error);
+	}
+
+}
+
 if ( (isset($_POST['name'])) && ($_POST['name'] != null)){
 
 	$Name = get_post($mysqli , 'name');
@@ -83,28 +93,6 @@ Length in minutes : <input type = 'text' name = 'length' size = '20' > <br>
 </form>
 <br>";
 
-/*if (isset($_REQUEST['Filter'])){
-
-	$query = "SELECT category FROM Inventory ";
-	$result	= $mysqli->query($query);
-	if (!$result) {
-		die ("Database access failed: " . $mysqli->error);
-	}
-
-	$row = $result->num_rows;
-	$RowInt = intval($row);
-
-	$CatArray[] = '';
-	
-	for ($b = $RowInt-1 ; $b >= 0 ; $b--){
-
-		$result->data_seek($b);
-		$row = $result->fetch_array(MYSQLI_NUM);
-		$CatArray[$b] = $row[0];
-	}
-
-	$FilterArr = array_unique($CatArray);
-}*/
 $query = "SELECT category FROM Inventory ";
 $result	= $mysqli->query($query);
 if (!$result) {
@@ -132,13 +120,10 @@ echo '
 
 foreach($FilterArr as $option){
 	echo "<option value = '$option' name = 'option'";
-	/*if ($_REQUEST['Filter'] == $option){
-		echo "selected='selected' ";
-	}*/
 	echo " >$option </option>";
 }
 echo '
-<option value = "All" selected = "selected" >All Catigories</option>
+<option value = "All" selected = "selected" >All Movies</option>
 </select>
 	<input type=submit value = "Filter" >
 </form>
@@ -172,7 +157,7 @@ if (isset($_REQUEST['Filter']) ) {
 		<th>Length</th>
 		<th>Availablity</th>";
 	
-	for($m = $RowInt-1 ; $m >= 0 ; $m--)
+	for($m = 0 ; $m < $RowInt ; $m++)
 	{
 		echo "<tr>";
 		$result ->data_seek($m);
@@ -212,75 +197,6 @@ if (isset($_REQUEST['Filter']) ) {
 	echo "
 	</table>
 	</div>";
-
-
-
-/*
-	$query = "SELECT * FROM Inventory ";
-	$result	= $mysqli->query($query);
-	if (!$result) {
-		die ("Database access failed: " . $mysqli->error);
-	}
-
-	$row = $result->num_rows;
-
-	$RowInt = intval($row);
-
-	echo "<br><br>
-	<div>
-	<table>
-		<tr> 
-		<th>ID</th>
-		<th>Video Name</th>
-		<th>Category</th>
-		<th>Length</th>
-		<th>Availablity</th>";
-
-
-	for($j = $RowInt-1 ; $j >= 0 ; $j--)
-	{
-
-		$result ->data_seek($j);
-		$row = $result->fetch_array(MYSQLI_NUM);
-
-		echo "<tr>";
-
-		for($k = 0 ; $k < 5 ; $k++)
-		{
-			if($k === 4){
-				if($row[$k] === '0'){
-					echo "<td>Available</td>";
-				}else
-					echo "<td>Rented</td>";
-			}
-			else
-				echo "<td>$row[$k]</td>";
-		}
-		echo "
-		<td>
-			<form action = 'videos.php' method = 'POST'>
-				<input type='hidden' name = 'delete' value = 'yes'>
-				<input type='hidden' name = 'id' value='$row[0]'>
-				<input type='submit' value='Delete Video' >
-			</form>
-		</td>" ;
-		echo "
-		<td>
-			<form action = 'videos.php' method = 'POST'>
-				<input type='hidden' name = 'switch' value = 'yes'>
-				<input type='hidden' name = 'id' value='$row[0]'>
-			<input type='submit' value='checkin / checkout' >
-			</form>
-		</td>" ;
-		echo "</tr>";
-
-	}
-	echo "
-	</table>
-	</div>";*/
-	
-	$result->close();
-	$mysqli->close();
 
 	}else{
 
@@ -345,30 +261,18 @@ if (isset($_REQUEST['Filter']) ) {
 
 	}
 
+	echo "
+<div>			
+<form action = 'videos.php' method = 'POST'>
+<input type='hidden' name = 'deleteAll' value = 'yes'>
+<input type='hidden' name = 'Filter' value='All'>
+<input type='submit' value='Delete All Video' >
+</form>
+</div>";
+
+	$mysqli->close();
+
 	function get_post($mysqli , $var){
 		return $mysqli->real_escape_string($_POST[$var]);
 	}
-
-/*if ($_POST){
-	$Name = $_POST['name'];
-	$Category = $_POST['category'];
-	$Length = $_POST['length'];
-
-		echo "Name is : " . $Name . "  " . "Category is " . $Category . "  " . "Length is " . $Length;
-	}
-	// Prepared statement
-	if (!($stmt = $mysqli->prepare("INSERT INTO Inventory(name,category,length) VALUES (?,?,?)"))) {
-		echo "Prepare statement failed: (" . $mysqli->error;
-	}
-
-	if (!$stmt->bind_param("ssi" , $Name , $Category, $Length)){
-		echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-	}
-
-	if (!$stmt->execute()) {
-		//echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-		echo "\nInvalid Entry: " . $stmt->error;
-	}*/
-
-//}
 ?>
